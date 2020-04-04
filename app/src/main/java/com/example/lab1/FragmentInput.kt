072fd10b75eba129283bebbed209b676309864a8
suodbons <rivanota@gmail.com>
2020-04-04 19:15:51 +0300

    refactoring

--- app/src/main/java/com/example/lab1/FragmentInput.kt
+++ app/src/main/java/com/example/lab1/FragmentInput.kt
@@ -3,24 +3,25 @@ package com.example.lab1
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_input.*
 import kotlinx.android.synthetic.main.fragment_input.view.*
 
 class FragmentInput : Fragment() {
-    lateinit var comm: TextUpdater
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_input, container, false)
-        comm = activity as TextUpdater
+        childFragmentManager.beginTransaction().add(R.id.fragment_default_picture, FragmentDefaultPicture()).commit()
         rootView.editText.addTextChangedListener(object : TextWatcher {
            override fun afterTextChanged(s: Editable?) {
                if (rootView.editText.text.length <= 120) {
-                   comm.passText(rootView.editText.text.toString())
+                   (activity as TextUpdater).passText(rootView.editText.text.toString())
                } else {
                    rootView.editText.setText(rootView.editText.text.take(120))
                    rootView.editText.setSelection(120)
@@ -29,6 +30,39 @@ class FragmentInput : Fragment() {
            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) { }
        })
+
+        rootView.button_ok.setOnClickListener{
+            val id = fonts.checkedRadioButtonId
+            if(id != -1){
+                if(editText.text.any()) {
+                    val radio: RadioButton = fonts.findViewById(id)
+                    (activity as MainActivity).updateFragment(radio, editText.text.toString())
+                    editText.clearFocus()
+                }
+                else{
+                    (activity as MainActivity).message("Enter text")
+                }
+            }
+            else{
+                (activity as MainActivity).message("Choose font")
+            }
+        }
+
+        rootView.button_cancel.setOnClickListener{
+            if(editText.text.isNotEmpty() || fonts.checkedRadioButtonId != -1)  {
+                if(editText.text.isNotEmpty())  {
+                    editText.text = null
+                    editText.clearFocus()
+                }
+                if(fonts.checkedRadioButtonId != -1){
+                    fonts.clearCheck()
+                }
+            }
+            else{
+                (activity as MainActivity).message("Nothing to cancel")
+            }
+            (activity as MainActivity).clear()
+        }
         return rootView
     }
 }
--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.example.lab1
 
 import android.os.Bundle
-import android.view.View
 import android.widget.RadioButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.fragment_input.*
+import androidx.fragment.app.Fragment
 
 
 class MainActivity : AppCompatActivity(), TextUpdater{
@@ -14,63 +13,41 @@ class MainActivity : AppCompatActivity(), TextUpdater{
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         if(savedInstanceState == null){
-            supportFragmentManager.beginTransaction().replace(R.id.fragment_input, FragmentInput()).commit()
-            supportFragmentManager.beginTransaction().replace(R.id.fragment_default_picture, FragmentDefaultPicture()).commit()
+            supportFragmentManager.beginTransaction().add(R.id.fragment_input, FragmentInput()).commit()
         }
     }
     override fun passText(input_text: String) {
-        var outputFragment =  supportFragmentManager.fragments[1]
-        outputFragment.arguments?.putString("input_txt", input_text)
-        supportFragmentManager.beginTransaction().detach(outputFragment).attach(outputFragment).commit()
+        if(supportFragmentManager.fragments.size > 1){
+            val outputFragment =  supportFragmentManager.fragments[1]
+            if (outputFragment is FragmentOutput){
+                outputFragment.arguments?.putString("input_txt", input_text)
+                supportFragmentManager.beginTransaction().detach(outputFragment).attach(outputFragment).commit()
+            }
+        }
     }
-
-    fun acceptOk(view: View){
-        val id = fonts.checkedRadioButtonId
-        if(id != -1){
-            if(editText.text.any()) {
-                val radio:RadioButton = findViewById(id)
-                var outputFragment = supportFragmentManager.fragments[1]
-                if (outputFragment is FragmentOutput){
-                    if(outputFragment.arguments?.getString("input_radio_id") != radio.text.toString()){
-                        if(outputFragment.arguments?.getString("input_radio_id") != radio.text.toString()){
-                            outputFragment.arguments?.putString("input_radio_id", radio.text.toString())
-                        }
-                        supportFragmentManager.beginTransaction().detach(outputFragment).attach(outputFragment).commit()
-                    }
-                    else{
-                        Toast.makeText(this, "Nothing changed", Toast.LENGTH_SHORT).show()
-                    }
-                }
-                else{
-                    outputFragment = FragmentOutput.newInstance(editText.text.toString(), radio.text.toString())
-                    supportFragmentManager.beginTransaction().replace(R.id.fragment_default_picture, outputFragment).commit()
-                    supportFragmentManager.fragments[0].arguments?.apply {
-                        putBoolean("update", true)
-                    }
-                    editText.clearFocus()
-                }
+    fun updateFragment(radio: RadioButton, text: String){
+        var outputFragment = Fragment()
+        if(supportFragmentManager.fragments.size > 1){
+            outputFragment = supportFragmentManager.fragments[1]
+        }
+        if (outputFragment is FragmentOutput){
+            if(outputFragment.arguments?.getString("input_radio_id") != radio.text.toString()){
+                outputFragment.arguments?.putString("input_radio_id", radio.text.toString())
+                supportFragmentManager.beginTransaction().detach(outputFragment).attach(outputFragment).commit()
             }
             else{
-                Toast.makeText(this, "Enter text", Toast.LENGTH_SHORT).show()
+                message("Nothing changed")
             }
         }
         else{
-            Toast.makeText(this, "Choose font", Toast.LENGTH_SHORT).show()
+            outputFragment = FragmentOutput.newInstance(text, radio.text.toString())
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_default_picture, outputFragment).commit()
         }
     }
-    fun cancel(view: View){
-        if(editText.text.isNotEmpty() || fonts.checkedRadioButtonId != -1)  {
-            if(editText.text.isNotEmpty())  {
-                editText.text = null
-                editText.clearFocus()
-            }
-            if(fonts.checkedRadioButtonId != -1){
-                fonts.clearCheck()
-            }
-        }
-        else{
-            Toast.makeText(this, "Nothing to cancel", Toast.LENGTH_SHORT).show()
-        }
+    fun clear(){
         supportFragmentManager.beginTransaction().replace(R.id.fragment_default_picture, FragmentDefaultPicture()).commit()
+    }
+    fun message(message: String){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
